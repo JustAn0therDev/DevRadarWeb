236f1fa0a98f550072b1f7fd34d47713f4772fd3
Ruan Montelo <ruanmontelo@gmail.com>
2020-01-31 08:51:02 -0300

    Inserindo botão de deleção adicionado.

--- src/components/DevItem/index.js
+++ src/components/DevItem/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import api from '../../services/api';
+import './styles.css';
 
 export default function DevItem({ dev }) {
+    const deleteDev = async (github_username) => {
+        await api.delete('/devs', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+          },
+          data: {
+            github_username
+          }
+        });
+
+        api.get('/devs');
+    }
+
+    const updateDev = async () => {
+
+    }
+
     return (
         <li className="dev-item">
         <header>
@@ -12,6 +32,10 @@ export default function DevItem({ dev }) {
         </header>
         <p>{!dev.biography ? "Este usuario nao possui biografia." : dev.biography}</p>
         <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
+        <div id="modify-buttons">
+          <button type="button" className="dev-button">Atualizar</button>
+          <button type="button" className="dev-button" onClick={(ev) => { ev.preventDefault(); deleteDev(dev.github_username) }}>Excluir</button>
+        </div>
       </li>
     )
 }
